672ae7322c443bf4b994766d9e4f1684a1275ec6
hyorimcho <hyorimm@gmail.com>
2023-05-09 20:53:58 +0900

    feat: 거래 취소 요청시 옵션 수정

--- src/lib/hooks/useChangeStatusMutate.ts
+++ src/lib/hooks/useChangeStatusMutate.ts
@@ -1,11 +1,10 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { changeSalesStatus } from "api";
-import React from "react";
 
 const useChangeStatusMutate = () => {
   const queryClient = useQueryClient();
   const { mutate: changeStatusMutate } = useMutation(
-    (id: string) => changeSalesStatus(id, true, true),
+    (id: string) => changeSalesStatus(id, true, false),
     {
       onSuccess: () => {
         queryClient.invalidateQueries();
